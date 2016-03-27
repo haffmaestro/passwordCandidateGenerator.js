2b2db0cf86f5578733e7bd04f89092cdc0f0db89
Halfdan Hem <halfdanhem@gmail.com>
2016-03-27 16:52:47 -0700

    feat(candidate generation)
    script that generates password candidates based on an array
    of possible permutations

---
+++ permutate.js
@@ -0,0 +1,75 @@
+var fs      = require('fs');
+var path    = require('path');
+var yargs   = require('yargs').argv;
+var assert  = require('assert');
+
+var defaultRecipePath = path.join(__dirname, '/permutations.json');
+var defaultResultPath = path.join(__dirname, '/candidates.json');
+
+var recipePath = yargs.recipe || defaultRecipePath;
+var resultPath = yargs.result || defaultResultPath;
+
+var recipe = JSON.parse(fs.readFileSync(recipePath,'utf8'));
+
+var results = []
+
+run();
+function run(){
+  generateTree("", recipe);
+  assert.equal(countPermutations(recipe), results.length);
+
+  fs.writeFile(resultPath, JSON.stringify(results, null, 4), function(err){});
+  console.log("Successfully generated: "+results.length+" candidates");
+}
+
+/**
+ * Counts the possible permutations for a given recipe
+ * @param  {Array} recipe  The recipe for generating candidates
+ * @return {Number}        [description]
+ */
+function countPermutations(recipe){
+  return recipe.map(function(optionsForPosition){
+    return optionsForPosition.length;
+  }).reduce(function(memo, optionNumber){
+    return memo*optionNumber;
+  });
+}
+
+/**
+ * Check that given array is an array of arrays;
+ * @param  {Array} array [description]
+ * @return {Boolean}       [description]
+ */
+function checkTypes(array){
+  return array.map(function(element){
+    return Array.isArray(element);
+  })
+  .reduce(function(memo, bool){
+    return memo && bool;
+  }, true);
+}
+
+/**
+ * Generates candidates recursively based on given recipe;
+ * @param  {String} sofar Candidate so far
+ * @param  {Array}  array Rest of recipe
+ */
+function generateTree(sofar, array){
+  if(!array.length) {
+    results.push(sofar)
+    return;
+  }
+
+  if(!checkTypes(array)){
+    console.warn("Every position in the given array is not an array");
+    return;
+  } 
+
+  var current = array[0];
+  var rest = array.slice(1);
+
+  current.forEach(function(candidatePart){
+    generateTree(sofar+candidatePart, rest);
+  })
+
+}
